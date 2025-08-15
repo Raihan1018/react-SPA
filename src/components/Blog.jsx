b7c1c5ccc5d0284e4e68afa149ba5e264ad4c99a
Raihan <raihanuddinahmed10@gmail.com>
2025-08-15 21:31:41 +0600

    all data displayed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { CiBookmark } from "react-icons/ci";
 
 const Blog = ({ blog }) => {
   const {
@@ -12,12 +12,43 @@ const Blog = ({ blog }) => {
   } = blog;
   console.log(blog);
   return (
-    <div>
+    <div className="p-5">
       <img
-        className="rounded-md m-5"
+        className="rounded-md w-full"
         src={cover_img}
         alt={`cover image of ${title}`}
       />
+
+      <div className="flex justify-between my-5">
+        <div className="flex items-center space-x-3">
+          <img className="rounded-full w-[60px]" src={author_img} alt="" />
+          <div>
+            <h3>{author_name}</h3>
+            <h5>{post_date}</h5>
+          </div>
+        </div>
+        <div>
+          <span className="flex items-center">
+            {reading_time} min read
+            <CiBookmark className="text-xl ml-2 text-gray-400 cursor-pointer hover:scale-125 transition-all duration-150" />
+          </span>
+        </div>
+      </div>
+      {/* title */}
+      <h2 className="text-3xl">{title}</h2>
+      {/* hash tag */}
+      <p className="space-x-2 text-gray-400">
+        {hash_tag.map((hash, index) => (
+          <span key={index}>
+            <a href="">{hash}</a>
+          </span>
+        ))}
+      </p>
+      <div>
+        <button className="hover:text-gray-500 transition-all duration-150">
+          Mark as read
+        </button>
+      </div>
     </div>
   );
 };
